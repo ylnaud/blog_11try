69b50886cd668bb655c1d073f8c0539f8c2359c1
ylnaud <thyronemiguelvegasantana@gmail.com>
2024-08-11 19:30:34 +0100

    ad

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
 
+  form.addEventListener("submit", function (event) {
+    const email = form.querySelector('input[name="email"]').value;
+    const firstName = form.querySelector('input[name="first_name"]').value;
+
+    if (!email || !firstName) {
+      event.preventDefault();
+      alert("Please fill out all required fields.");
+    }
+
+    // Aquí podrías agregar lógica adicional, como validación de email
+  });
+});
