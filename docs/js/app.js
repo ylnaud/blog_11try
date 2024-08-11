1b1f90e12044230b36f6c6b145cc5900cc0e9bfd
ylnaud <thyronemiguelvegasantana@gmail.com>
2024-08-11 19:13:14 +0100

    &a

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  const handleSubmit = (event) => {
-    event.preventDefault();
 
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-    .then(() => console.log("Formulario enviado exitosamente"))
-    .catch((error) => alert("Error al enviar el formulario: " + error));
-  };
-
-  document.querySelector("form").addEventListener("submit", handleSubmit);
-});
